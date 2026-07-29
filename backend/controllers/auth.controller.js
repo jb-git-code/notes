2e7b5db2ed5b43d080ac8303e210341b986b3aa4
jb-git-code <jayanshub18@gmail.com>
2026-07-29 20:04:34 +0530

    create user login api

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -24,17 +24,54 @@ export const registerUser = asyncHandler(async (req, res) => {
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    const user = User.create({
+    const user = await User.create({
         name,
         email,
-        hashedPassword,
+        password:hashedPassword,
     });
-
+    
     res.status(201).json({
         success: true,
         message: "User registered successfully",
         data: {
-            id: user._id,
+            name: user.name,
+            email: user.email,
+        },
+    });
+});
+
+export const loginUser = asyncHandler(async(req , res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({
+            success: false,
+            message: "Email and password are required",
+        });
+    }
+
+    const user = await User.findOne({email});
+
+    if (!user) {
+        return res.status(401).json({
+            success: false,
+            message: "Invalid email or password",
+        });
+    }
+
+    const isMatch = await bcrypt.compare(password , user.password);
+
+    if (!isMatch) {
+        return res.status(401).json({
+            success: false,
+            message: "Invalid email or password",
+        });
+    }
+
+    res.status(201).json({
+        success: true,
+        message: "User Logged-in successfully",
+        data: {
             name: user.name,
             email: user.email,
         },
